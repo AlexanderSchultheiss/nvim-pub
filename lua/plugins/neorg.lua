c45a0877fd7f1315bdd6c2401a06418d7e2cca29
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2025-09-11 19:30:02 +0200

    add image, neorg, undotree, and zenmode

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "nvim-neorg/neorg",
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.summary"] = {},
+          ["core.latex.renderer"] = {},
+          ["core.journal"] = { -- Manages Neorg workspaces
+            config = {
+              workspace = "journal",
+              journal_folder = "logs",
+            },
+          },
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                journal = "~/journal/",
+              },
+              default_workspace = "journal",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
