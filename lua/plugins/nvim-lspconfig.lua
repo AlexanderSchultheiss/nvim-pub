dad1b9eee3437bff9c5a445fc88990b204ba5a4e
Alexander Schultheiß <alexander.schultheiss@metiundo.io>
2025-12-08 08:33:39 +0100

    switch to vstls

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -16,12 +16,12 @@ return {
         -- explicitly add default filetypes, so that we can extend
         -- them in related extras
         filetypes = {
-          --          "javascript",
-          --          "javascriptreact",
-          --          "javascript.jsx",
-          --          "typescript",
-          --          "typescriptreact",
-          --          "typescript.tsx",
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
         },
         settings = {
           complete_function_calls = true,
