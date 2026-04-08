c5c38656dc1507fa9c49862ee31d3d35b92622a3
Alexander Schultheiß <alexander.schultheiss@metiundo.io>
2026-04-08 10:19:51 +0200

    options: tsgo as default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,12 @@ opt.scrolloff = 8
 
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+
+-- LSP Server to use for TypeScript.
+---@type "vtsls" | "tsgo"
+-- vim.g.lazyvim_ts_lsp = "vtsls" -- currently the default
+vim.g.lazyvim_ts_lsp = "tsgo" -- currently the default
+
+-- To use the newer, much faster `tsgo` LSP server, either:
+-- * enable the `tsgo` extra, or
+-- * set `vim.g.lazyvim_ts_lsp = "tsgo"` in your `options.lua`
