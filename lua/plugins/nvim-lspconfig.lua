3c28fc3d34b91bc0c6d8e5b0281b609a29d1efd1
Alexander Schultheiß <alexander.schultheiss@metiundo.io>
2026-04-08 10:21:55 +0200

    Revert "back to default config"
    This reverts commit a9e89af7cdd8d48946fb2ace4e5e0c49138f9b54.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,4 +1,4 @@
-if true then
+if false then
   return {}
 end
 
@@ -7,16 +7,25 @@ return {
   opts = {
     -- make sure mason installs the server
     servers = {
+      --- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
+      --- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
+      tsserver = {
+        enabled = false,
+      },
+      ts_ls = {
+        enabled = false,
+      },
       vtsls = {
+        enabled = true,
         -- explicitly add default filetypes, so that we can extend
         -- them in related extras
         filetypes = {
-          "javascript",
-          "javascriptreact",
-          "javascript.jsx",
-          "typescript",
-          "typescriptreact",
-          "typescript.tsx",
+          --          "javascript",
+          --          "javascriptreact",
+          --          "javascript.jsx",
+          --          "typescript",
+          --          "typescriptreact",
+          --          "typescript.tsx",
         },
         settings = {
           complete_function_calls = true,
@@ -71,9 +80,19 @@ return {
             desc = "File References",
           },
           {
+            "<leader>co",
+            LazyVim.lsp.action["source.organizeImports"],
+            desc = "Organize Imports",
+          },
+          {
             "<leader>cM",
             LazyVim.lsp.action["source.addMissingImports.ts"],
             desc = "Add missing imports",
+          },
+          {
+            "<leader>cu",
+            LazyVim.lsp.action["source.removeUnused.ts"],
+            desc = "Remove unused imports",
           },
           {
             "<leader>cD",
@@ -83,11 +102,7 @@ return {
           {
             "<leader>cV",
             function()
-              LazyVim.lsp.execute({
-                title = "Select TypeScript Version",
-                filter = "vtsls",
-                command = "typescript.selectTypeScriptVersion",
-              })
+              LazyVim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
             end,
             desc = "Select TS workspace version",
           },
@@ -95,7 +110,39 @@ return {
       },
     },
     setup = {
+      --- @deprecated -- tsserver renamed to ts_ls but not yet released, so keep this for now
+      --- the proper approach is to check the nvim-lspconfig release version when it's released to determine the server name dynamically
+      tsserver = function()
+        -- disable tsserver
+        return true
+      end,
+      ts_ls = function()
+        -- disable tsserver
+        return true
+      end,
       vtsls = function(_, opts)
+        if vim.lsp.config.denols and vim.lsp.config.vtsls then
+          ---@param server string
+          local resolve = function(server)
+            local markers, root_dir = vim.lsp.config[server].root_markers, vim.lsp.config[server].root_dir
+            vim.lsp.config(server, {
+              root_dir = function(bufnr, on_dir)
+                local is_deno = vim.fs.root(bufnr, { "deno.json", "deno.jsonc" }) ~= nil
+                if is_deno == (server == "denols") then
+                  if root_dir then
+                    return root_dir(bufnr, on_dir)
+                  elseif type(markers) == "table" then
+                    local root = vim.fs.root(bufnr, markers)
+                    return root and on_dir(root)
+                  end
+                end
+              end,
+            })
+          end
+          resolve("denols")
+          resolve("vtsls")
+        end
+
         Snacks.util.lsp.on({ name = "vtsls" }, function(buffer, client)
           client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
             ---@type string, string, lsp.Range
